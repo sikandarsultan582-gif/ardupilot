39732ef4f4770e3276ee1469cb9c82da16590d78
sikandarsultan582-gif <sikandarsultan582@gmail.com>
2026-03-01 03:40:17 +0430

    Update mode_auto.cpp

--- ArduPlane/mode_auto.cpp
+++ ArduPlane/mode_auto.cpp
@@ -13,6 +13,7 @@ float get_terminal_pitch(const Location &loc1, const Location &loc2) {
     if (angle > 0) return -8500.0f; 
     return angle;
 }
+
 bool ModeAuto::_enter()
 {
 #if HAL_QUADPLANE_ENABLED
